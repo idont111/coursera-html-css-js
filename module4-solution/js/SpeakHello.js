46a5cd46627a1e631029d9a414b5d5a7c6b676eb
Alina Chernysh <55505572+idont111@users.noreply.github.com>
2022-05-05 21:51:40 +0300

    Update SpeakHello.js

--- module4-solution/js/SpeakHello.js
+++ module4-solution/js/SpeakHello.js
@@ -1,7 +1,6 @@
 (function (window) {  
     var helloSpeaker = {};
     var speakWord = "Hello";
-    
     helloSpeaker.speak = function (names) {
       console.log(speakWord + " " + names);
     }
